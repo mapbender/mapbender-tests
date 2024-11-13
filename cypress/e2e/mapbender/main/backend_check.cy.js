1747758d41f6bfd3fb4aa9ea957978266c5802d7
Arno Fleckenstein <arno.fleckenstein@wheregroup.com>
2024-11-13 13:27:55 +0100

    Title and heading strings removed

--- cypress/e2e/mapbender/main/backend_check.cy.js
+++ cypress/e2e/mapbender/main/backend_check.cy.js
@@ -12,17 +12,20 @@ describe('check_backend_main_functions', () => {
 
     it('Check main functions', () => {
         cy.copyApplication({ _title: myAppTitle, _slug: myAppSlug });
+
         cy.visit('http://localhost/mapbender4/index.php/');
         cy.wait(2000);
+
         cy.visit('http://localhost/mapbender4/index.php/manager/repository/');
         cy.wait(2000);
+
         cy.visit('http://localhost/mapbender4/index.php/manager/security');
         cy.wait(2000);
-        cy.visit('http://localhost/mapbender4/');
-        cy.contains('Mapbender Demo Cypress').scrollIntoView({ offset: { top: -200, left:0 } });
-        //cy.contains('Mapbender Demo Cypress').scrollIntoView({ block: 'center', offset: { top: -100, left:0 } });
 
+        cy.visit('http://localhost/mapbender4/');
+        cy.contains(myAppTitle).scrollIntoView({ offset: { top: -200, left:0 } });
         cy.wait(2000);
+
         cy.deleteApplication({ _slug: myAppSlug });
     });
 
