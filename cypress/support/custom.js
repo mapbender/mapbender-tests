467653dc2b766f96cb83d64c5ce890ea1a502cd4
Arno Fleckenstein <arno.fleckenstein@wheregroup.com>
2024-11-21 11:30:16 +0100

    format and add data-test attributes

--- cypress/support/custom.js
+++ cypress/support/custom.js
@@ -5,14 +5,12 @@ Cypress.Commands.add('login', (benutzer) => {
     const url = Cypress.env('application')['mainUrl'];
     cy.visit(url);
     cy.get('ul[data-test="login"] > li').click();
-
     cy.get('input[name=_username]')
         .type(benutzer._username);
-
     cy.get('input[name=_password]')
         .type(benutzer._password);
-
-    cy.get('[type="submit"]').click();
+    //cy.get('[type="submit"]').click();
+    cy.get('input[data-test="mb-submit"]').click();
 });
 
 // duplicate Mapbender Demo App
@@ -20,9 +18,11 @@ Cypress.Commands.add('login', (benutzer) => {
 Cypress.Commands.add('copyApplication', (new_application) => {
     const url = Cypress.env('application')['mainUrl'] ;
     cy.visit(url + 'manager/application/mapbender_user/copydirectly' );
-    cy.get('#application_title').clear().type(new_application._title);
-    cy.get('#application_slug').clear().type(new_application._slug);
-    cy.get('input[type="submit"]').click();
+    cy.get('#application_title').clear()
+        .type(new_application._title);
+    cy.get('#application_slug').clear()
+        .type(new_application._slug);
+    cy.get('input[data-test="mb-submit"]').click();
 });
 
 // delete application
@@ -31,7 +31,7 @@ Cypress.Commands.add('deleteApplication', (app) => {
     const url = Cypress.env('application')['mainUrl']
     cy.visit(url);
     cy.get('[data-test="' + app._slug + '-delete"]').click();
-    cy.get('button').contains('Löschen').click();
+    cy.get('button[data-test="mb-submit"]').click();
 });
 
 // add User to Mapbender
@@ -40,11 +40,16 @@ Cypress.Commands.add( 'addMapbenderUser', (newUser)=> {
     const url = Cypress.env('application')['mainUrl'];
     cy.visit(url + 'manager/security');
     cy.get('[data-test="user-add"]').click();
-    cy.get('[id="user_username"]').type(newUser._username);
-    cy.get('[id="user_email"]').type(newUser._email);
-    cy.get('[id="user_password_first"]').type(newUser._password);
-    cy.get('[id="user_password_second"]').type(newUser._password);
-    cy.get('[value="Speichern"]').click();
+    cy.get('[id="user_username"]')
+        .type(newUser._username);
+    cy.get('[id="user_email"]')
+        .type(newUser._email);
+    cy.get('[id="user_password_first"]')
+        .type(newUser._password);
+    cy.get('[id="user_password_second"]')
+        .type(newUser._password);
+    //cy.get('[value="Speichern"]').click();
+    cy.get('input[data-test="mb-submit"]').click();
 })
 
 // delete User from Mapbender
@@ -52,10 +57,8 @@ Cypress.Commands.add( 'addMapbenderUser', (newUser)=> {
 Cypress.Commands.add('deleteMapbenderUser', (userName) => {
     const url = Cypress.env('application')['mainUrl'];
     cy.visit(url + 'manager/security');
-    cy.get('span[data-test="' + userName + ' - delete"]').click();
-    //cy.visit(url + 'manager/security');
-    //cy.get('[data-url="' + visitUrl + '"]').click();
-    cy.contains('[type="button"]' ,'Löschen').click();
+    cy.get('span[data-test="' + userName + '-user-delete"]').click();
+    cy.get('button[data-test="mb-submit"]').click();
 
 })
 
