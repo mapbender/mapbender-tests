6564b72eeaf673aa5511c4b19d4f6c66a016f1ca
Arno Fleckenstein <arno.fleckenstein@wheregroup.com>
2024-12-09 08:03:00 +0100

    delete unnecessary code

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,26 +24,53 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+// login
+// cy.login({ _username: 'root', _password: 'root' })
+Cypress.Commands.add('login', (benutzer) => {
+    const url = Cypress.env('application')['mainUrl'];
+    cy.visit(url);
+    cy.get('ul[data-test="login"] > li').click();
+    cy.get('input[name=_username]')
+        .type(benutzer._username);
+    cy.get('input[name=_password]')
+        .type(benutzer._password);
+    //cy.get('[type="submit"]').click();
+    cy.get('input[data-test="mb-submit"]').click();
+});
 
+Cypress.Commands.add('showBanner', (message, duration = 3000) => {
+    cy.mbLog('showBannder: Start');
+    cy.document().then((doc) => {
+        // Prüfen, ob ein Banner bereits existiert
+        let banner = doc.getElementById('cypress-banner');
+        if (!banner) {
+            // Neues Banner erstellen
+            banner = doc.createElement('div');
+            banner.id = 'cypress-banner';
+            banner.style.position = 'fixed';
+            banner.style.top = '0';
+            banner.style.left = '0';
+            banner.style.width = '100%';
+            banner.style.backgroundColor = '#000';
+            banner.style.color = '#fff';
+            banner.style.padding = '10px';
+            banner.style.textAlign = 'center';
+            banner.style.zIndex = '9999';
+            doc.body.appendChild(banner);
+        }
+        // Nachricht aktualisieren
+        banner.innerText = message;
 
-
-// add source
-// cy.addSource({ _url: 'https://www.wms.nrw.de/wms/krankenhaus?' })
-Cypress.Commands.add('addSource', (source) => {
-    cy.get('a')
-        .eq(5)
-        .click()
-    cy.url().should('include', '/repository')
-    // click on the plus
-    cy.get('.iconAdd.iconBig.right').click()
-    cy.url().should('include', '/repository/new')
-    cy.get('#http_source_origin_originUrl').type(source._url)
-    cy.get('.button')
-        .contains('Load')
-        .click()
-})
-
-
+        // Banner nach einer bestimmten Zeit entfernen (optional)
+        if (duration > 0) {
+            setTimeout(() => {
+                if (banner) {
+                    banner.remove();
+                }
+            }, duration);
+        }
+    });
+});
 
 // close Symfony Toolbar
 // cy.closeSymfonyToolbar()
@@ -51,43 +78,5 @@ Cypress.Commands.add('closeSymfonyToolbar', (source) => {
     cy.visit('/')
     cy.get('[class="hide-button"]').click()
 })
-/* Muss überarbeitet werden
-// add source to layerset main
-// cy.addSourceToLayerset({ _title: 'image_export_bug_01', _name: 'Krankenhäuser NRW'})
-Cypress.Commands.add('addSourceToLayerset', (application, source) => {
-        cy.visit('/manager/application/${application._title}')
-        //cy.contains('Filter').click().type(application._title)
-        //cy.get['class[title="Edit"]'].click({force: true})
-
-        cy.get('a[title="Edit application ${application._title}"]').click()
-        cy.get('#tabLayers').click()
-        cy.get('a[data-layerset-title*="main"]').click()
-        cy.get('#inputFilterSources').type('source._name')
-        cy.get('.subTitle')
-          .contains('source._name').click({force: true}) // Timed out retrying: cy.click() failed because this element: is being covered by another element:
-        cy.contains('Save').click()
-        cy.contains('Back').click()
-//      cy.visit('/application/new_application._title')
-})
-*/
-
-/*
-// Image Snapshot Plugin
-import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
-
-addMatchImageSnapshotCommand({
-    failureThreshold: 0.01,
-    failureThresholdType: 'percent',
-    customDiffConfig: { threshold: 0.0 },
-    capture: 'viewport',
-});
 
 
-Cypress.Commands.add("setResolution", (size) => {
-    if (Cypress._.isArray(size)) {
-        cy.viewport(size[0], size[1]);
-    } else {
-        cy.viewport(size);
-    }
-})
-*/
