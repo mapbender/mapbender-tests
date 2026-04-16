2e3b2bbe70c507421e4a452b8ccf95e93aeddefb
Arno Fleckenstein <arno.fleckenstein@wheregroup.com>
2026-04-16 13:23:34 +0200

    delete WMTS and vector tiles too

--- cypress/e2e/mapbender/01-backend/02-sources/08-deleteDataSource.cy.js
+++ cypress/e2e/mapbender/01-backend/02-sources/08-deleteDataSource.cy.js
@@ -9,19 +9,40 @@ describe('Delete Data Source', () => {
         cy.login({_username: user, _password: password});
     })
 
-    const dataSourceTitle = 'WMS NW ALKIS';
+    const dataSourceTitleWMS = 'WMS NW ALKIS';
+    const dataSourceTitleWMTS = 'WMTS NW ALKIS';
+    const dataSourceTitleVT = 'OSM Liberty';
     const waitLong= 2000;
     it('test delete data source', () => {
         cy.CyLog('Test delete Data Source', 'Start');
         const url = Cypress.env('application')['mainUrl'];
         cy.visit(url + 'manager/repository');
 
-        cy.get('a[data-test="mb-delete-source-' + dataSourceTitle + '"]').then($elem =>{
+        // WMS Service delete
+        cy.get('a[data-test="mb-delete-source-' + dataSourceTitleWMS + '"]').then($elem =>{
             const wmsLength = $elem.length;
             for (let i = 0; i < $elem.length; i++){
-                cy.deleteMapbenderSource(dataSourceTitle);
+                cy.deleteMapbenderSource(dataSourceTitleWMS);
             }
         });
+
+        // WMTS Service delete
+        cy.get('a[data-test="mb-delete-source-' + dataSourceTitleWMTS + '"]').then($elem =>{
+            const wmsLength = $elem.length;
+            for (let i = 0; i < $elem.length; i++){
+                cy.deleteMapbenderSource(dataSourceTitleWMTS);
+            }
+        });
+
+        // Vector tile Service delete
+        cy.get('a[data-test="mb-delete-source-' + dataSourceTitleVT + '"]').then($elem =>{
+            const wmsLength = $elem.length;
+            for (let i = 0; i < $elem.length; i++){
+                cy.deleteMapbenderSource(dataSourceTitleVT);
+            }
+        });
+
+
         cy.wait(waitLong);
         cy.CyLog('Test delete Data Source', 'End');
     });
